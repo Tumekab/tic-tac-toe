021bc28bf22b96749d4cb969980d46fc935f7742
Tumeka Burston <burston1993@icloud.com>
2021-01-28 21:25:21 +0000

    split up cell css styling into cell.css

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -1,36 +1,20 @@
 import React from 'react';
 import './Board.css';
 
+import { Cell } from '../Cell/Cell'
+
 export const Board = () => {
   return (
     <div id="board">
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
-        <button className="cell">
-            <span className="cell-content"></span>
-        </button>
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
+        <Cell />
     </div>
   );
 }
